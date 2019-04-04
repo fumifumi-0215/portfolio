bc496a679df4ba756135b13f8d2f380d26835a70
Fumiaki Okubo <hcno1.0215@gmail.com>
2019-04-04 12:03:14 +0000

    Finish static page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+    
+    # ページごとにタイトルを返す。
+  def full_title(page_title = "")
+    base_title = "画像アップロードシステム"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
