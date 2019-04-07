3d7e16b0f5006e10d4c769af28657d59b650d7ee
Fumiaki Okubo <hcno1.0215@gmail.com>
2019-04-07 14:46:46 +0000

    basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get  '/signup', to: 'users#new'
+  get  '/signup',   to: 'users#new'
+  get    '/login',  to: 'sessions#new'
+  post   '/login',  to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   root 'static_pages#home'
   resources :users
 end
